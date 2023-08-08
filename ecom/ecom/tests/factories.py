0067b50a563daa303f5dab299b4f993ed8a23f3b
nishant21n <118267294+nishant21n@users.noreply.github.com>
2023-08-08 23:01:01 +0530

    nc

---
+++ ecom/ecom/tests/factories.py
@@ -0,0 +1,28 @@
+import factory
+
+from ecom.product.models import Brand, Category, Product
+
+
+class CategoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Category
+
+    name = factory.Sequence(lambda n: "Category_%d" % n)
+
+
+class BrandFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Brand
+
+    name = factory.Sequence(lambda n: "Brand_%d" % n)
+
+
+class ProductFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Product
+
+    name = factory.Sequence(lambda n: "Product_%d" % n)
+    description = "test_description"
+    is_digital = True
+    brand = factory.SubFactory(BrandFactory)
+    category = factory.SubFactory(CategoryFactory)
